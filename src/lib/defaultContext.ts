ff66e1fac71dfb525121be434500d554814b808f
Migueljc123 <migueljcbrbm@gmail.com>
2026-07-02 13:51:24 -0300

    feat: ajustando link do github

--- src/lib/defaultContext.ts
+++ src/lib/defaultContext.ts
@@ -5,7 +5,7 @@ export const DEFAULT_CONTEXT: ProfissionalContext = {
   titulo: 'Desenvolvedor Full-Stack Júnior',
   email: 'migueljccardozo@gmail.com',
   telefone: '+55 (24) 99973-6248',
-  github: 'github.com/Migueljc123',
+  github: 'github.com/Migueljc213',
   linkedin: 'www.linkedin.com/in/jose-miguelfjc',
   portfolio: 'portifolio-jose-miguel.vercel.app',
   localizacao: 'Rio de Janeiro, RJ',
